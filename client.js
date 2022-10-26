d16388a1733f0772f80db7677f2b78d01603a69d
Will Wadman <wadmanwill@gmail.com>
2022-10-26 17:16:15 +0000

    Updates to es6 formatting:

---
+++ client.js
@@ -0,0 +1,35 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+
+  });
+  conn.on("connect", (data) => {
+    // code that does something when the connection is first established
+    console.log('Connected to game server');
+    conn.on('data', (msg) => {
+
+      console.log(msg);
+
+
+    });
+
+    conn.setEncoding("utf8");
+
+    return conn;
+
+  });
+};
+console.log("Connecting ...");
+
+
+module.exports = {
+connect,
+}
+
+
+
+
--- play.js
+++ play.js
@@ -1,32 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-    
-      
-    });
-    conn.on("connect", (data) => {
-      // code that does something when the connection is first established
-console.log('Connected to game server');
-conn.on('data', (msg) => {
-  
-  console.log(msg);
-
-
-});
-  
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-
-  });
-};
-  console.log("Connecting ...");
+const {connect} = require('./client.js')
 connect();
 
 /*
